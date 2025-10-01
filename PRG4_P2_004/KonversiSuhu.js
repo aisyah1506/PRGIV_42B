08bedc55a1d0096b0d635b9cbf297361ff2bfbc9
aisyah1506 <aimnpyd@gmail.com>
2025-10-02 04:36:20 +0700

    first commit

---
+++ PRG4_P2_004/KonversiSuhu.js
@@ -0,0 +1,51 @@
+function konversiSuhu(nilai, jenis) {
+    let hasil;
+
+    switch (jenis) {
+        case "C/F": // Celsius ke Fahrenheit
+            hasil = (nilai * 9 / 5) + 32;
+            break;
+
+        case "C/R": // Celsius ke Reamur
+            hasil = nilai * 4 / 5;
+            break;
+
+        case "F/C": // Fahrenheit ke Celsius
+            hasil = (nilai - 32) * 5 / 9;
+            break;
+
+        case "F/R": // Fahrenheit ke Reamur
+            hasil = (nilai - 32) * 4 / 9;
+            break;
+
+        case "R/C": // Reamur ke Celsius
+            hasil = nilai * 5 / 4;
+            break;
+
+        case "R/F": // Reamur ke Fahrenheit
+            hasil = (nilai * 9 / 4) + 32;
+            break;
+
+        default:
+            hasil = "Jenis konversi tidak valid";
+    }
+
+    return hasil;
+}
+
+function mulaiKonversi() {
+    let nilai = parseFloat(document.getElementById("suhu").value);
+    let jenis = document.getElementById("konversi").value;
+
+    if (isNaN(nilai)) {
+        document.getElementById("hasilKonversi").innerHTML = "❌ Masukkan angka yang valid.";
+        return;
+    }
+
+    let hasil = konversiSuhu(nilai, jenis);
+
+    document.getElementById("hasilKonversi").innerHTML = 
+        <b>${nilai}</b> dikonversi (${jenis}) = <b>${hasil.toFixed(2)}</b>;
+}
+
+
